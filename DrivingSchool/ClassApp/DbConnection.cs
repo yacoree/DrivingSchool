3f517fde57555572869404c542adb92ba31bfecc
212014 <212014@1220-002-22>
2022-10-04 15:07:13 +0300

    Добавьте файлы проекта.

---
+++ DrivingSchool/ClassApp/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrivingSchool;
+    
+
+namespace DrivingSchool.ClassApp
+{
+    internal class DbConnection
+    {
+        public static AutoschollEntities Connection = new AutoschollEntities();
+    }
+}
---
+++ DrivingSchool/PageApp/AutoPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DrivingSchool.ClassApp;
+namespace DrivingSchool.PageApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoPage.xaml
+    /// </summary>
+    public partial class AutoPage : Page
+    {
+        public AutoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegPage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if((TextLogin.Text !="")&&(TextPassword.Password !=""))
+                    {
+                var DataLogin = DbConnection.Connection.Logins.Where
+                    (z=>z.login==TextLogin.Text && z.password == TextPassword.Password).FirstOrDefault();
+                if(DataLogin!=null)
+                {
+                    MessageBox.Show($"{DataLogin}","Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Correct \n password");
+                }
+            }
+        }
+    }
+}
